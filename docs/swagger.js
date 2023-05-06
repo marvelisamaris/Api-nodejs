b707fcb6ec63dfbeeda7f5ef120daefda0cd54b4
ClaseGIT <marvelis690@gmail.com>
2023-05-05 23:41:59 -0500

    PROBANDO

--- docs/swagger.js
+++ docs/swagger.js
@@ -13,7 +13,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: "http://localhost:4000/api"
+                url: "http://localhost:3004/api"
             },
             {
                 url: "http://localhost:3000/api"
